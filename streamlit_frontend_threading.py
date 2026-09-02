05533ad45f8a5f4d5fcff2227dbceb57ccefd88e
ankit-k-sah16 <ankitkumar040920@gmail.com>
2026-09-03 02:36:51 +0530

    Adding files to the repo.

--- streamlit_frontend_threading.py
+++ streamlit_frontend_threading.py
@@ -5,17 +5,35 @@ import uuid
 
 # ********************************* Utility Functions *******************************************
 
-# Generate a unique thread ID for each new chat session
+# Generates a unique thread ID for each new chat session
 def generate_thread_id():
     thread_id = uuid.uuid4()
 
-    return thread_id
+    return str(thread_id)
 
+# Resets the chat session by clearing the message history and generating a new thread ID
 def reset_chat():
     thread_id = generate_thread_id()
+
     st.session_state['thread_id'] = thread_id
+
+    add_thread_to_history(st.session_state['thread_id'])
+
     st.session_state['message_history'] = []
 
+# Adds a thread ID to the history of chat threads
+def add_thread_to_history(thread_id):
+
+    if thread_id not in st.session_state['chat_threads']:
+        st.session_state['chat_threads'].append(thread_id)
+
+#
+def load_conversation_history(thread_id):
+    state = chatbot.get_state(
+        config={"configurable": {"thread_id": thread_id}}
+    )
+
+    return state.values.get("messages", [])
 
 # ********************************* Session Setup ***********************************************
 
@@ -25,28 +43,59 @@ if 'message_history' not in st.session_state:
 if 'thread_id' not in st.session_state:
     st.session_state['thread_id'] = generate_thread_id()
 
+if "chat_threads" not in st.session_state:
+    st.session_state["chat_threads"] = []
+
+add_thread_to_history(st.session_state['thread_id'])
 
 # ********************************* Sidebar UI **************************************************
 
 st.sidebar.title("LangGraph Chatbot")
+
 st.sidebar.button("New Chat",on_click=reset_chat, type="primary")
+
 st.sidebar.header("Previous Conversations")
-#st.sidebar.text(st.session_state['message_history'][-1]['content'] if st.session_state['message_history'] else "No previous conversations.")
-st.sidebar.text(st.session_state['thread_id'])
+
+for thread_id in st.session_state['chat_threads'][::-1 ]:  # Display threads in reverse order (most recent first)
+
+    if st.sidebar.button(thread_id):
+
+        st.session_state['thread_id'] = thread_id
+
+        messages = load_conversation_history(thread_id)
+
+        temp_messages = []
+
+        for message in messages:
+
+            if isinstance(message, HumanMessage):
+                role = 'user'
+
+            else:
+                role = 'assistant'
+            
+            temp_messages.append({"role": role, "content": message.content})
+
+        st.session_state['message_history'] = temp_messages
+        st.rerun()  # Refresh the app to display the loaded conversation
+
 
 # ********************************* Main UI ******************************************************
 
 # Loading conversation history
 for message in st.session_state['message_history']:
+
     with st.chat_message(message['role']):
-        st.text(message['content'])
+        st.markdown(message['content'])
+
+# Chat Input
 
 user_input = st.chat_input("Type your message here...")
 
 
 if user_input :
 
-    # Adding user message to message history
+    # Adding user message to UI history
     st.session_state['message_history'].append({"role": "user", "content": user_input})
 
     
@@ -54,14 +103,18 @@ if user_input :
         st.text(user_input)
 
     CONFIG ={ "configurable": {"thread_id": st.session_state['thread_id'] }}
+
+
     # Response Generation
     with st.chat_message("assistant"):
 
         ai_message = st.write_stream(
-            message_chunk.content for message_chunk , metadata in chatbot.stream ({ 'messages': [HumanMessage(content = user_input)]},
+            message_chunk.content for message_chunk , metadata in chatbot.stream(
+            { 'messages': [HumanMessage(content = user_input)]},
             config = CONFIG,
             stream_mode = "messages"
         )
     )
-    # Adding assistant response to message history
+        
+    # Adding assistant response to UI history
     st.session_state['message_history'].append({"role": "assistant", "content": ai_message})
