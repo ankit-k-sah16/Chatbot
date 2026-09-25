dd8d3ea53ad9c048170dcd262dbd0864dc51e52b
ankit-k-sah16 <ankitkumar040920@gmail.com>
2026-09-25 16:18:26 +0530

    "Modified the tool backend file and created a mcp client file"

--- chatbot_async.py
+++ chatbot_async.py
@@ -1,6 +1,5 @@
 from langgraph.graph import StateGraph,START,END
 from langchain_groq import ChatGroq
-from langchain_community.tools import DuckDuckGoSearchRun
 from typing import TypedDict,Annotated
 from langchain_core.messages import HumanMessage,BaseMessage
 from langchain_core.tools import tool
@@ -10,6 +9,7 @@ from langgraph.prebuilt import ToolNode,tools_condition
 from dotenv import load_dotenv
 import sqlite3
 import requests
+import asyncio
 
 load_dotenv()  # Load environment variables from .env file
 #-------------------------------------------------
@@ -21,8 +21,6 @@ llm = ChatGroq(model="openai/gpt-oss-120b",temperature=0.6)
 #-------------------------------------------------
 # 2. Tool Setup
 #-------------------------------------------------
-# Tools
-search_tool = DuckDuckGoSearchRun(region='us-en')
 
 @tool
 def calculator_tool(first_num:float, sec_num:float, operation:str,) -> dict:
@@ -48,18 +46,7 @@ def calculator_tool(first_num:float, sec_num:float, operation:str,) -> dict:
     except Exception as e:
         return f"Error: {str(e)}"
 
-@tool
-def get_stock_price(symbol: str) -> dict:
-    """"
-    Fetch latest stock price for a given symbol (eg.AAPL,MSFT)
-    from Alpha Vantage using the API given in the url
-    
-    """
-    url="" 
-    res=requests.get(url)
-    return res.json()
-
-tools=[search_tool,calculator_tool,get_stock_price]
+tools=[calculator_tool]
 
 llm_with_tool = llm.bind_tools(tools)
 
@@ -69,42 +56,41 @@ llm_with_tool = llm.bind_tools(tools)
 class ChatState(TypedDict):
     messages: Annotated[list[BaseMessage], add_messages]
 
-#-------------------------------------------------
-# 4. Node Setup
-#-------------------------------------------------
-def chat_node(state: ChatState):
-    "LLM node that answers the user query by ownself or using the given tools"
-    messages = state['messages']
-    response = llm_with_tool.invoke(messages)
-    return {'messages':response}
 
-t_node = ToolNode(tools)
+def build_graph():
 
-#-------------------------------------------------
-# 5. Checkpointer Setup
-#-------------------------------------------------
-conn = sqlite3.connect(database='langgraph_chatbot.db',check_same_thread=False)
-checkpointer = SqliteSaver(conn=conn)
+    async def chat_node(state: ChatState):
 
-#-------------------------------------------------
-# 6. Graph Setup
-#-------------------------------------------------
-graph = StateGraph(ChatState)
-graph.add_node('chat_node',chat_node)
-graph.add_node('tools',t_node)
+        "LLM node that answers the user query by ownself or using the given tools"
+        messages = state['messages']
+        response = await  llm_with_tool.ainvoke(messages)
+        return {'messages':response}
 
-graph.add_edge(START,'chat_node')
-graph.add_conditional_edges('chat_node', tools_condition)
+    t_node = ToolNode(tools)
 
-graph.add_edge('tools','chat_node')
+    #-------------------------------------------------
+    # 5. Graph Setup
+    #-------------------------------------------------
+    graph = StateGraph(ChatState)
+    graph.add_node('chat_node',chat_node)
+    graph.add_node('tools',t_node)
 
-chatbot = graph.compile(checkpointer=checkpointer)
+    graph.add_edge(START,'chat_node')
+    graph.add_conditional_edges('chat_node', tools_condition)
 
-#-------------------------------------------------
-# 7. Helper Functions
-#-------------------------------------------------
-def retrieve_all_thread_ids():
-    all_thread = set()
-    for checkpoint in checkpointer.list(None):
-        all_thread.add(checkpoint.config['configurable']['thread_id'])
-    return list(all_thread) 
+    graph.add_edge('tools','chat_node')
+
+    chatbot = graph.compile()
+    return chatbot
+
+async def main():
+
+    chatbot = build_graph()
+
+    # running the graph
+    result = await chatbot.ainvoke({"messages": [HumanMessage(content="Find the modulus of 132354 and 23 and give answer like a cricket commentator")]})
+
+    print(result['messages'][-1].content)
+
+if __name__ == "__main__":
+    asyncio.run(main())
